a4196443a51d9fff7253ec5ea6d4a422c154b014
Thiago Morais Cordeiro <thiago.morais@intellissis.com.br>
2025-04-15 15:46:31 -0300

    projeto finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo:"Modelo1", imei:"444444444", memoria: 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\nSmartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "654321", modelo:"Modelo2", imei:"555555555", memoria: 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("TikTok");
